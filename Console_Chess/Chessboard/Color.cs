6bad938420b4d0c45ce153bc74ca4330bcc47a40
JrD <jrd.learn@gmail.com>
2022-05-24 12:14:09 -0300

    Working on Chessboard

---
+++ Console_Chess/Chessboard/Color.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Chessboard
+{
+    internal enum Color
+    {
+        White,
+        Black,
+        Yellow,
+        Blue,
+        Red,
+        Green,
+        Orange
+    }
+}
---
+++ Console_Chess/Chessboard/Piece.cs
@@ -0,0 +1,18 @@
+﻿namespace Chessboard
+{
+    internal class Piece
+    {
+        public Position position { get; set; }
+        public Color color { get; protected set; }
+        public int numMoves { get; protected set; }
+        public Chessboard board { get; protected set; }
+
+        public Piece(Position position, Chessboard board, Color color)
+        {
+            this.position = position;
+            this.board = board;
+            this.color = color;
+            this.numMoves = 0;
+        }
+    }
+}
