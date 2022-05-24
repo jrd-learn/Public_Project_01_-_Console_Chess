e061c51d1b7230d0e80f724fdf50f6e1aae51c78
JrD <jrd.learn@gmail.com>
2022-05-24 11:58:47 -0300

    Add Position Class

--- Console_Chess/Program.cs
+++ Console_Chess/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Console_Chess
+﻿using System;
+using System.Globalization;
+using Chessboard;
+
+namespace Console_Chess
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Position P;
+            P = new Position(3, 4);
+
+            Console.WriteLine($"Posição: {P}");
         }
     }
 }
